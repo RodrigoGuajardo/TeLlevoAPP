b51a4d20756f87075c090a328b1f13cd9da09bdd
Rodrigo Guajardo <ror.guajardo@duocuc.cl>
2024-09-13 04:37:35 -0300

    "reparaciones en secciones del codigo"

--- src/app/pedir/pedir.page.ts
+++ src/app/pedir/pedir.page.ts
@@ -23,12 +23,14 @@ export class PedirPage implements OnInit {
       document.documentElement.style.setProperty("--fondo", "#212121");
       document.documentElement.style.setProperty("--fondo-input", "#1d2b2f");
       document.documentElement.style.setProperty("--texto-input", "#848C9F");
+      document.documentElement.style.setProperty("--textos", "#848C9F");
 
       this.icono = "claro"
     }else{
       document.documentElement.style.setProperty("--fondo", "#00ffd9");
       document.documentElement.style.setProperty("--fondo-input", "#00ffd9");
       document.documentElement.style.setProperty("--texto-input", "#1b1b1b");
+      document.documentElement.style.setProperty("--textos", "#1b1b1b");
 
       this.icono = "oscuro"
     }
@@ -36,12 +38,12 @@ export class PedirPage implements OnInit {
 
   ngOnInit(){
     this.anim.create()
-    .addElement(document.querySelector("#vehicle")!)
+    .addElement(document.querySelector("#iconVehicle")!)
     .duration(2000)
     .iterations(Infinity)
     .direction("alternate")
     .fromTo("color", "#0010f5", "#36029e")
-    .fromTo("transform", "scale(1) rotate(-10deg)", "scale(1.5) rotate(10deg)")
+    .fromTo("transform", 'translateX(100px)', 'translateX(-100px)')
     .play()
     
   }
