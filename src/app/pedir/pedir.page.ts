4591405004e3703a5f819be461f2e8a1dd2e99cd
Rodrigo Guajardo <ror.guajardo@duocuc.cl>
2024-09-13 03:46:23 -0300

    "integracion de pedir, ademas de intentos de x cosas"

--- src/app/pedir/pedir.page.ts
+++ src/app/pedir/pedir.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pedir',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedirPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  icono   = "oscuro"
+  usuario = ""
+  clave   = ""
+
+  constructor(
+    private anim:AnimationController
+  ) { }
+
+
+  cambiarTema(){
+    if(this.icono == "oscuro"){
+      document.documentElement.style.setProperty("--fondo", "#212121");
+      document.documentElement.style.setProperty("--fondo-input", "#1d2b2f");
+      document.documentElement.style.setProperty("--texto-input", "#848C9F");
+
+      this.icono = "claro"
+    }else{
+      document.documentElement.style.setProperty("--fondo", "#00ffd9");
+      document.documentElement.style.setProperty("--fondo-input", "#00ffd9");
+      document.documentElement.style.setProperty("--texto-input", "#1b1b1b");
+
+      this.icono = "oscuro"
+    }
+  }
+
+  ngOnInit(){
+    this.anim.create()
+    .addElement(document.querySelector("#vehicle")!)
+    .duration(2000)
+    .iterations(Infinity)
+    .direction("alternate")
+    .fromTo("color", "#0010f5", "#36029e")
+    .fromTo("transform", "scale(1) rotate(-10deg)", "scale(1.5) rotate(10deg)")
+    .play()
+    
   }
 
 }
